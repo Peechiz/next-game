df105977937e8d19b824b9bb11062e61dedf672b
Chris Impicciche <chris.impicciche@customink.com>
2022-11-18 16:05:14 -0600

    add log

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,17 @@
 // import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import '../styles/globals.css'
-import { Prisma } from '@prisma/client'
 
 import HomePage, { GameWithTags } from './HomePage'
 
 async function getGames() {
-  const res = await fetch('http://' + process.env.VERCEL_URL + '/api/games')
-  const games: GameWithTags[] = await res.json();
-  return games;
+  try {
+    const res = await fetch('http://' + process.env.VERCEL_URL + '/api/games')
+    const games: GameWithTags[] = await res.json();
+    return games;
+  } catch (err) {
+    console.log('ERRuR', err)
+  }
 }
 
 export default async function Page() {
