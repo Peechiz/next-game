ccc04c59671bfe3ce9c0ea7ba2132aa0f95c60b2
Chris Impicciche <chris.impicciche@customink.com>
2022-11-18 17:53:29 -0600

    but like seriously why

--- app/page.tsx
+++ app/page.tsx
@@ -5,29 +5,15 @@ import '../styles/globals.css'
 
 import HomePage, { GameWithTags } from './HomePage'
 
-let baseUrl: string;
-if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
-  baseUrl = 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL
-} else {
-  baseUrl = 'http://' + process.env.NEXT_PUBLIC_VERCEL_URL
-}
-
-async function getGames() {
-  const res = await fetch( baseUrl + '/api/games')
-  const games: GameWithTags[] = await res.json();
-  return games;
-}
-
 export default async function Page() {
-  const games = await getGames();
   return (
     <div className={styles.container}>
 
       <main className={styles.main}>
         <h1>Why, hello there</h1>
         <p>{process.env.NEXT_PUBLIC_VERCEL_ENV}</p>
-        <p>{baseUrl}/api/games</p>
-        {/* <HomePage games={games} /> */}
+        {/* <p>{baseUrl}/api/games</p> */}
+        <HomePage />
       </main>
     </div>
   )
