d8b9e766746db8ca1746948ff4b7e3dc9c4c66ae
Chris Impicciche <chris.impicciche@customink.com>
2022-11-18 18:04:02 -0600

    ok screw using fetch

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -1,39 +1,30 @@
 'use client';
 import {Prisma} from '@prisma/client'
-import { useState } from 'react'
+// import { useState } from 'react'
 
 const gameWithTags = Prisma.validator<Prisma.GameArgs>()({
   include: { tags: true }
 })
 export type GameWithTags = Prisma.GameGetPayload<typeof gameWithTags>
 
-let baseUrl: string;
-if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
-  baseUrl = 'https://' + process.env.NEXT_PUBLIC_VERCEL_URL
-} else {
-  baseUrl = 'http://' + process.env.NEXT_PUBLIC_VERCEL_URL
-}
-
-
 
 // This is a Client Component. It receives data as props and
 // has access to state and effects just like Page components
 // in the `pages` directory.
-export default function HomePage() {
-  const [games, setGames] = useState<GameWithTags[]>([])
+export default function HomePage({ games }: { games: GameWithTags[] } ) {
+  // const [games, setGames] = useState<GameWithTags[]>([])
 
-  async function getGames() {
-    const res = await fetch( baseUrl + '/api/games')
-    const games: GameWithTags[] = await res.json();
-    setGames(games)
-  }
+  // async function getGames() {
+  //   const res = await fetch( baseUrl + '/api/games')
+  //   const games: GameWithTags[] = await res.json();
+  //   setGames(games)
+  // }
 
   return (
     <div>
       {games?.map((game) => (
         <div key={game.id}>{game.metadata.name}</div>
       ))}
-      <button onClick={() => getGames()}>get games</button>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,33 @@
 // import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import '../styles/globals.css'
+import prisma from '../db/prisma'
+import {Prisma} from '@prisma/client'
 
-import HomePage, { GameWithTags } from './HomePage'
+import { makeSerializable } from '../db/util'
+
+const gameWithTags = Prisma.validator<Prisma.GameArgs>()({
+  include: { tags: true }
+})
+export type GameWithTags = Prisma.GameGetPayload<typeof gameWithTags>
+
+import HomePage from './HomePage'
+
+async function getGames(): Promise<GameWithTags[]> {
+  const res = await prisma.game.findMany({ include: {
+    tags: true
+  }})
+  return makeSerializable(res)
+}
 
 export default async function Page() {
+  const games = await getGames();
   return (
     <div className={styles.container}>
 
       <main className={styles.main}>
         <h1>Why, hello there</h1>
-        <p>{process.env.NEXT_PUBLIC_VERCEL_ENV}</p>
-        {/* <p>{baseUrl}/api/games</p> */}
-        <HomePage />
+        <HomePage games={games}/>
       </main>
     </div>
   )
